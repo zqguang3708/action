b8a0c5ca7e58e518c92bd0a215bf4a8a61b82a5b
zqguang3708 <940233647@qq.com>
2021-09-07 13:48:35 +0800

    Add files via upload

---
+++ action.py
@@ -0,0 +1,2 @@
+import requests
+requests.get('https://api.htm.fun/api/Wechat/text/?corpid=ww0c3359f9ab041a08&corpsecret=UlNcNxTv_RtDrbO7BWV66hFUBGkaxWwC6o5HPcTgKxY&agentid=1000002&text=Github Action')
